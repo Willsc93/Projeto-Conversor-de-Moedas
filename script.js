29f43bfd25bdad4149861bf6d638aab349a82ef5
Willsc93 <williansc93@gmail.com>
2025-02-20 15:14:26 -0300

    PROJETO CONVERSOR DE MOEDAS

---
+++ script.js
@@ -0,0 +1,33 @@
+const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
+
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
+
+    const currencyValueToConverted = document.querySelector(".currency-value")
+
+    console.log("currencySelect.value")
+
+    const dolarToday = 5.2
+
+    const euroToday = 6.2
+
+    const convertedValue = inputCurrencyValue / dolarToday
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+    currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(convertedValue)
+
+
+
+}
+
+convertButton.addEventListener("click", convertValues)
