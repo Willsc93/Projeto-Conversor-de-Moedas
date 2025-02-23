5c6bea7b8b213a8df4e1b4325f73d255be022805
Willsc93 <williansc93@gmail.com>
2025-02-22 22:22:33 -0300

    Correções

--- script.js
+++ script.js
@@ -6,28 +6,35 @@ function convertValues() {
 
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
 
-    const currencyValueToConverted = document.querySelector(".currency-value")
+    const currencyValueConverted = document.querySelector(".currency-value")
 
-    console.log("currencySelect.value")
+    console.log(currencySelect.value)
 
     const dolarToday = 5.2
 
     const euroToday = 6.2
 
-    const convertedValue = inputCurrencyValue / dolarToday
+    if (currencySelect.value == "dolar") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+     
+    if (currencySelect.value == "euro") {
+            currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+                style: "currency",
+                currency: "EUR"
+            }).format(inputCurrencyValue / euroToday)
+
+        }
+    
 
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL"
     }).format(inputCurrencyValue)
-
-    currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD"
-    }).format(convertedValue)
-
-
-
 }
+
 
 convertButton.addEventListener("click", convertValues)
